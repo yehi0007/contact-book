3722a495587aafca04781cf062a6ddc6586d7340
Habiba Yehia <yehi0007@algonquinlive.com>
2026-08-11 01:30:19 -0400

    Fix GitHub Pages routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import AddContact from "../views/AddContact.vue";
 import EditContact from "../views/EditContact.vue";
@@ -12,7 +12,7 @@ const routes = [
 ];
 
 const router = createRouter({
-history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
